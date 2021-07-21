c4a9a63f5cb9f3ad4613449ade9b80c51c0dfaec
Rachel Mroz <mroz.rachelj@gmail.com>
2021-07-21 13:45:49 -0500

    added label to story board and editor

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -16,6 +16,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aboutMeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
