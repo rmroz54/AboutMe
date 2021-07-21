ffc805cb451cc0e318bbf1abf35f763ccded0856
Rachel Mroz <mroz.rachelj@gmail.com>
2021-07-21 13:38:18 -0500

    version

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by rmroz on 7/21/21.
+//
+
+//this is now under version control
+
+//MME summit 
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
